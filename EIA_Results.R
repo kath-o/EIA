67d1128088b3c92cc7991b9382115f5592e1cf45
Kathleen Owen <kathleenowen@MacBook-Air-309.local>
2024-11-25 13:07:13 +0000

    installing package and loading data

--- EIA_Results.R
+++ EIA_Results.R
@@ -1,4 +1,11 @@
 #RESULTS FOR ECOLOGICAL IMPACT ASSESSMENT
 
+#graphical representations of biodiversity: number of orders present comparison 
+
+#loading data: vertebrates
+#install.packages("readxl")
+library(readxl)
+
+verts <- read_excel("~/Desktop/MSc EEB/WD/EIA/arran_res.xlsx",sheet="Vertebrates")
 
 
